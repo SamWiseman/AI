d7956210c0a2528a9048d0904abb131d6310f36b
Leila Awad <leilaawad@eduroam-174-144.dyn.carleton.edu>
2018-02-17 14:32:50 -0600

    now we can cross validate and do predictions, but no prec and rec yet

--- osha.py
+++ osha.py
@@ -6,13 +6,12 @@ from sklearn import tree
 from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
 import graphviz 
+from copy import deepcopy
 
 def main():
-	decisionTree()
-
-def decisionTree():
 	X = []
 	Y = []
+	#read file into arrays and preprocess data
 	data = open('HW3_Data.txt')
 	for line in data:
 		example = line.split()
@@ -25,7 +24,6 @@ def decisionTree():
 		X.append(example)
 	X.pop(0)
 	Y.pop(0)
-
 	for i in range (len(Y)):
 		if Y[i] == "Compliant":
 			Y[i] = 1
@@ -34,17 +32,50 @@ def decisionTree():
 		else:
 			Y[i] = 0
 
-	clf = tree.DecisionTreeClassifier()
-	clf = clf.fit(X, Y)
+	crossValidate(10, X, Y)
 
-	print(clf.predict([[53.76,9,0],[35.87,35,0]]))
+def crossValidate(k, X, Y):
+	totalLength = len(Y)
+	splitLength = totalLength // k
+	for i in range(k):
+		trainingX = deepcopy(X)
+		trainingY = deepcopy(Y)
+		testingXRange = trainingX[i * splitLength:] if i == k - 1\
+			else trainingX[i * splitLength:(i + 1) * splitLength] 
+		testingX = deepcopy(testingXRange)
+		trainingX = trainingX[:i*splitLength] if i == k - 1 else \
+			trainingX[:i * splitLength] + trainingX[(i + 1) * splitLength:]
+		testingYRange = trainingY[i * splitLength:] if i == k - 1\
+			else trainingY[i * splitLength:(i + 1) * (splitLength)]
+		testingY = deepcopy(testingYRange)
+		trainingY = trainingY[:i * splitLength] if i == k - 1 else \
+			trainingY[:i * splitLength] + trainingY[(i + 1) * splitLength:]
+		output = "visulation k = " + str(i + 1)
+		decisionTree(trainingX, trainingY, testingX, testingY, output)
+
+
+
+def decisionTree(trainingX, trainingY, testingX, testingY, output):
+	
+	clf = tree.DecisionTreeClassifier()
+	clf = clf.fit(trainingX, trainingY)
+	prediction = clf.predict(testingX)
+	if not len(testingY) == len(prediction):
+		print("Somebody fucked up. Yikes.")
+	for i in range(len(prediction)):
+		if prediction[i] == testingY[i]:
+			print("Cool! They're equal.")
+		else:
+			print("Oh well! They are not equal.")
+
+	#message = "We good!" if equal else "Hmmm..."
+	#print(message)
 
 	#create visual graph
 	dot_data = tree.export_graphviz(clf, out_file=None) 
 	graph = graphviz.Source(dot_data) 
-	graph.render("osha")
+	graph.render(output)
 
-	#now we need to do predictions and such?
 
 	
 	 
