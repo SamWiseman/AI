f3e5577aea8dba8a70acebe8e6c153cba8b49c4f
SamWiseman <wisemans@carleton.edu>
2018-03-06 10:27:22 -0600

    it's better but i can't figure out how to recursion this :(

--- csp.py
+++ csp.py
@@ -4,6 +4,50 @@ from copy import deepcopy
 
 def main():
 	#initialize rooms
+	bruteActions = getBruteForceSolution()
+	
+
+def getBruteForceSolution():
+	roomList = generateGraph()
+	actions = [0, 1, 2]
+	root = State([None] * len(roomList), [])
+	solutions = []
+	bruteForce(root, actions, 0, roomList, solutions)
+	return solutions
+
+#our brute force solution
+def bruteForce(state, actions, roomNum, roomList, solutions):
+	print("state actions:", state.getActions())
+	if roomNum == len(roomList) - 1:
+		print("Full coloring complete.")
+		goodActions = state.getActions()
+		print("Solution:", goodActions)
+		solutions.append(goodActions)
+		return []
+	for i in range(len(actions)):
+		print("i is", i)
+		if validChild(roomList[roomNum], actions[i]) == True:
+			print("valid")
+			#recurse here? want a child with the list of actions filled in so far
+			child = State(deepcopy(state.actions), [])
+			action = actions[i]
+			child.setAction(roomNum, action)
+			roomList[roomNum].setAction(actions)
+			state.addChild(child)
+		else:
+			print("It is a not valid to color", roomList[i].getName(), "with", actions[i])
+	state.printState()
+	for child in state.getChildren():
+		print("child:")
+		child.printState()
+	
+def validChild(room, action):
+	for neighbor in room.getNeighbors():
+		if neighbor.getAction() == action:
+			return False
+	return True
+	
+def generateGraph():
 	w1 = Room("Warehouse 1")
 	w2 = Room("Warehouse 2")
 	w3 = Room("Warehouse 3")
@@ -25,36 +69,7 @@ def main():
 	o5.addNeighbors([w1, o4, w4, o6])
 	o6.addNeighbors([o1, o5, w4])
 	#list of variables such that each room is adjacent to the previous
-	actions = [0, 1, 2]
-	roomList = [w1, o1, o6, o5, o4, o2, o3, w2, w3, w4]
-	bruteForce(roomList, actions)
-
-
-
-
-def bruteForce(roomList, actions):
-	root = State([None] * len(roomList), [])
-	print("root actions:", root.getActions())
-	for i in range(len(actions)):
-		print("i is", i)
-		child = State(deepcopy(root.actions), [])
-		roomNum = 0
-		action = actions[i]
-		child.setAction(roomNum, actions)
-		roomList[roomNum].setAction(actions)
-		root.addChild(child)
-	print(root)
-	for child in root.getChildren():
-		print("child:")
-		child.printState()
-	
-def validChild(room, action):
-	for neighbor in room.getNeighbors():
-		if neighbor.getAction() == action:
-			return False
-	return True
-		
-
+	return [w1, o1, o6, o5, o4, o2, o3, w2, w3, w4]
 
 class Room:
 	def __init__(self, name):
